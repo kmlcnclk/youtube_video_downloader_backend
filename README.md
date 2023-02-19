d23ab2f90a2835fccd34304b68921d72d678181e
Kamilcan Çelik <kamilcancelik5757@gmail.com>
2023-02-19 13:13:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# youtube_video_downloader_backend
+ 
---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+
+app.get('/downloadmp3', async (req, res, next) => {
+  try {
+    var url = req.query.url;
+    if (!ytdl.validateURL(url)) {
+      return res.sendStatus(400);
+    }
+    let title = 'audio';
+
+    await ytdl.getBasicInfo(
+      url,
+      {
+        format: 'mp4',
+      },
+      (err, info) => {
+        if (err) throw err;
+        title = info.player_response.videoDetails.title.replace(
+          /[^\x00-\x7F]/g,
+          ''
+        );
+      }
+    );
+
+    res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
+    ytdl(url, {
+      format: 'mp3',
+      filter: 'audioonly',
+    }).pipe(res);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get('/downloadmp4', async (req, res, next) => {
+  try {
+    let url = req.query.url;
+    if (!ytdl.validateURL(url)) {
+      return res.sendStatus(400);
+    }
+    let title = 'video';
+
+    await ytdl.getBasicInfo(
+      url,
+      {
+        format: 'mp4',
+      },
+      (err, info) => {
+        title = info.player_response.videoDetails.title.replace(
+          /[^\x00-\x7F]/g,
+          ''
+        );
+      }
+    );
+
+    res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
+    ytdl(url, {
+      format: 'mp4',
+      filter: 'audioandvideo',
+      quality: 'highestvideo',
+    }).pipe(res);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at PORT=${PORT}`);
+});
